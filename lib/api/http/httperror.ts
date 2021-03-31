fbe3f6448e06b81ce011724a43dc871c7e196270
Apacheopteryx <Idevtanm@gmail.com>
2021-03-31 00:59:39 -0500

    better http errors

--- lib/api/http/httperror.ts
+++ lib/api/http/httperror.ts
@@ -1,6 +1,8 @@
+import { green, yellow } from "../../util/logger.ts";
+
 export interface ResponseError {
   code: number;
-  errors?: Record<string, { _errors: Omit<ResponseError, "errors">[] }>;
+  errors?: any; // TODO: get this type right
   message: string;
 }
 
@@ -13,12 +15,36 @@ export class HTTPError extends Error implements ResponseError {
   name = "HTTPError";
 
   constructor(body: ResponseError, public response: Response) {
-    super(body.message);
+    super();
 
     this.#message = body.message;
 
     this.code = body.code;
     this.errors = body.errors;
+  }
+
+  get message() {
+    const code = yellow(`${this.code}`);
+    const errors = this.formatErrors()
+      .replace(/'.+?'/g, (str) => green(str))
+      .replace(/\b\d+\b/g, (int) => yellow(int))
+      .slice(0, -1);
+    return `[${code}] ${this.#message}\n${errors}`;
+  }
+
+  private formatErrors(errors = this.errors, key = "") {
+    let str = "";
+    for (const k in errors) {
+      const thing = errors[k];
+      if (thing._errors) {
+        for (const e of thing._errors) {
+          str += `[${e.code}] ${key}${k}: ${e.message}\n`;
+        }
+      } else if (thing) {
+        str += this.formatErrors(errors[k], `${key}${k}.`);
+      }
+    }
+    return str;
   }
 }
 
