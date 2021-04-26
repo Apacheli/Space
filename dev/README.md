9783a38e9bb3b396cbe7a3abcec12c01ebb9fdef
Apacheopteryx <Idevtanm@gmail.com>
2021-04-25 23:27:10 -0500

    add dev scripts

---
+++ dev/README.md
@@ -0,0 +1,3 @@
+# Development Scripts
+
+Make sure to enable `--allow-read` and `--allow-write` when running these.
---
+++ dev/upgrade.ts
@@ -0,0 +1,21 @@
+import { parse } from "https://deno.land/std@0.95.0/path/mod.ts";
+import { version } from "../meta.ts";
+
+const stage = "alpha";
+
+let promptVersion = prompt("new version:");
+if (!promptVersion) {
+  throw new Error("bad new version");
+}
+const newVersion = `${promptVersion}${stage && `-${stage}`}`;
+
+const write = async (file: string) => {
+  const content = await Deno.readTextFile(file);
+  const newContent = content.replace(new RegExp(version, "g"), newVersion);
+  const { ext, name } = parse(file);
+  await Deno.writeTextFile(`${name}-new.${ext}`, newContent);
+};
+
+const files = ["meta.ts", "README.md"];
+
+await Promise.all(files.map((file) => write(file)));
