f6869cbaaf8664a03311527f2834b70e90036cc5
Apacheopteryx <Idevtanm@gmail.com>
2021-04-25 21:00:17 -0500

    thanks circular errors

--- lib/client/rest_client.ts
+++ lib/client/rest_client.ts
@@ -1,7 +1,21 @@
 import { HTTPClient, HTTPClientOptions } from "../api/http/mod.ts";
+import { channelPermissionsDecorator } from "../util/mod.ts";
+import { PermissionFlagsBits } from "../util/deps.ts";
 
 export interface RESTClientOptions extends HTTPClientOptions {
 }
 
 export class RESTClient extends HTTPClient {
+  constructor(
+    public client: any,
+    token: string,
+    options?: RESTClientOptions,
+  ) {
+    super(token, options);
+  }
+
+  @channelPermissionsDecorator(PermissionFlagsBits.SEND_MESSAGES)
+  createMessage(channelID: any, data: any, files?: any) {
+    return super.createMessage(channelID, data, files);
+  }
 }
