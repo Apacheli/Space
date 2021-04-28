f9e9b0f272f561aedab6bde6023a514194b2737a
Apacheopteryx <Idevtanm@gmail.com>
2021-04-27 21:46:01 -0500

    message HTTP in RestClient

--- lib/client/rest_client.ts
+++ lib/client/rest_client.ts
@@ -1,7 +1,10 @@
 import {
+  RESTGetAPIChannelMessageReactionUsersQuery,
   RESTGetAPIChannelMessagesQuery,
   RESTPatchAPIChannelJSONBody,
+  RESTPatchAPIChannelMessageJSONBody,
   RESTPostAPIChannelMessageJSONBody,
+  RESTPostAPIChannelMessagesBulkDeleteJSONBody,
 } from "./deps.ts";
 import { HTTPClient, HTTPClientOptions } from "../api/http/mod.ts";
 import { ActualSnowflake, channelPermissionsDecorator } from "../util/mod.ts";
@@ -47,7 +50,7 @@ export class RESTClient extends HTTPClient {
     return super.getChannelMessage(channelID, messageID);
   }
 
-  @channelPermissionsDecorator("VIEW_CHANNEL", "SEND_MESSAGES")
+  @channelPermissionsDecorator("SEND_MESSAGES", "VIEW_CHANNEL")
   createMessage(
     channelID: ActualSnowflake,
     data: RESTPostAPIChannelMessageJSONBody,
@@ -57,12 +60,111 @@ export class RESTClient extends HTTPClient {
   }
 
   @channelPermissionsDecorator(
-    "SEND_MESSAGES",
     "MANAGE_MESSAGES",
-    "VIEW_CHANNEL",
     "READ_MESSAGE_HISTORY",
+    "SEND_MESSAGES",
+    "VIEW_CHANNEL",
   )
   crosspostMessage(channelID: ActualSnowflake, messageID: ActualSnowflake) {
     return super.crosspostMessage(channelID, messageID);
+  }
+
+  @channelPermissionsDecorator(
+    "ADD_REACTIONS",
+    "READ_MESSAGE_HISTORY",
+    "VIEW_CHANNEL",
+  )
+  createReaction(
+    channelID: ActualSnowflake,
+    messageID: ActualSnowflake,
+    emoji: string,
+  ) {
+    return super.createReaction(channelID, messageID, emoji);
+  }
+
+  @channelPermissionsDecorator("READ_MESSAGE_HISTORY", "VIEW_CHANNEL")
+  deleteOwnReaction(
+    channelID: ActualSnowflake,
+    messageID: ActualSnowflake,
+    emoji: string,
+  ) {
+    return super.deleteOwnReaction(channelID, messageID, emoji);
+  }
+
+  @channelPermissionsDecorator(
+    "MANAGE_MESSAGES",
+    "READ_MESSAGE_HISTORY",
+    "VIEW_CHANNEL",
+  )
+  deleteUserReaction(
+    channelID: ActualSnowflake,
+    messageID: ActualSnowflake,
+    emoji: string,
+    userID: ActualSnowflake,
+  ) {
+    return super.deleteUserReaction(channelID, messageID, emoji, userID);
+  }
+
+  @channelPermissionsDecorator("READ_MESSAGE_HISTORY", "VIEW_CHANNEL")
+  getReactions(
+    channelID: ActualSnowflake,
+    messageID: ActualSnowflake,
+    emoji: string,
+    query: RESTGetAPIChannelMessageReactionUsersQuery,
+  ) {
+    return super.getReactions(channelID, messageID, emoji, query);
+  }
+
+  @channelPermissionsDecorator(
+    "MANAGE_MESSAGES",
+    "READ_MESSAGE_HISTORY",
+    "VIEW_CHANNEL",
+  )
+  deleteAllReactions(channelID: ActualSnowflake, messageID: ActualSnowflake) {
+    return super.deleteAllReactions(channelID, messageID);
+  }
+
+  @channelPermissionsDecorator(
+    "MANAGE_MESSAGES",
+    "READ_MESSAGE_HISTORY",
+    "VIEW_CHANNEL",
+  )
+  deleteAllReactionsforEmoji(
+    channelID: ActualSnowflake,
+    messageID: ActualSnowflake,
+    emoji: string,
+  ) {
+    return super.deleteAllReactionsforEmoji(channelID, messageID, emoji);
+  }
+
+  @channelPermissionsDecorator("READ_MESSAGE_HISTORY", "VIEW_CHANNEL")
+  editMessage(
+    channelID: ActualSnowflake,
+    messageID: ActualSnowflake,
+    data: RESTPatchAPIChannelMessageJSONBody,
+  ) {
+    // TODO: Check flag edits
+    return super.editMessage(channelID, messageID, data);
+  }
+
+  @channelPermissionsDecorator(
+    "MANAGE_MESSAGES",
+    "READ_MESSAGE_HISTORY",
+    "VIEW_CHANNEL",
+  )
+  deleteMessage(channelID: ActualSnowflake, messageID: ActualSnowflake) {
+    return super.deleteMessage(channelID, messageID);
+  }
+
+  @channelPermissionsDecorator(
+    "MANAGE_MESSAGES",
+    "READ_MESSAGE_HISTORY",
+    "VIEW_CHANNEL",
+  )
+  bulkDeleteMessages(
+    channelID: ActualSnowflake,
+    data: RESTPostAPIChannelMessagesBulkDeleteJSONBody,
+  ) {
+    return super.bulkDeleteMessages(channelID, data);
   }
 }
