c5aeb793f72c698b215fd46a3c68bae55dfe174b
Apacheopteryx <Idevtanm@gmail.com>
2021-04-27 22:31:27 -0500

    highlight permissions error

--- lib/client/permissions_error.ts
+++ lib/client/permissions_error.ts
@@ -1,9 +1,13 @@
+import { logger } from "../util/mod.ts";
+
 export default class PermissionsError extends Error {
   constructor(public permissions: string[]) {
     super();
   }
 
   get message() {
-    return `Missing permissions: ${this.permissions.join(", ")}`;
+    return `Missing permissions: ${
+      logger.highlight(`"${this.permissions.join('", "')}"`)
+    }`;
   }
 }
