22d801559cc764b2b7abfda11b019da402c26f22
Apacheopteryx <Idevtanm@gmail.com>
2021-05-02 15:09:28 -0500

    update decorators

--- lib/client/decorators.ts
+++ lib/client/decorators.ts
@@ -4,10 +4,16 @@ import type { GuildChannel } from "../structures/mod.ts";
 import type { ActualSnowflake } from "../util/util.ts";
 import { computePermissions } from "./permissions.ts";
 
-export const channelPermissionsDecorator = (
-  ...permissions: (keyof typeof PermissionFlagsBits)[]
-) => {
-  return (_target: unknown, _key: string, descriptor: PropertyDescriptor) => {
+export type Permissions = keyof typeof PermissionFlagsBits;
+
+export type PermissionsDecorator = (
+  ...permissions: Permissions[]
+) => (_target: unknown, _key: unknown, descriptor: PropertyDescriptor) => void;
+
+export const channelPermissionsDecorator: PermissionsDecorator = (
+  ...permissions
+) =>
+  (_target, _key, descriptor) => {
     const method = descriptor.value;
 
     descriptor.value = async function (
@@ -15,38 +21,31 @@ export const channelPermissionsDecorator = (
       channelID: ActualSnowflake,
       ...args: unknown[]
     ) {
-      const channel = await this.client.channels?.get(
-        channelID,
-      ) as GuildChannel;
+      const channel = <GuildChannel> await this.client.channels?.get(channelID);
       if (!(channel?.guildID && this.client.user)) {
         return method.call(this, channelID, ...args);
       }
-
       const guild = await this.client.guilds?.get(channel.guildID);
       const member = await guild?.members?.get(this.client.user.id);
       if (!(member && guild)) {
         return method.call(this, channelID, ...args);
       }
-
-      const currentUserPermissions = await computePermissions(
-        member,
-        guild,
-        channel,
+      const missingPermissions = computeMissingPermissions(
+        await computePermissions(member, guild, channel),
+        permissions,
       );
-      const missing = [];
-      for (const permission of permissions) {
-        if (
-          (currentUserPermissions & PermissionFlagsBits[permission]) !==
-            PermissionFlagsBits[permission]
-        ) {
-          missing.push(permission);
-        }
+      if (missingPermissions.length) {
+        throw new PermissionsError(missingPermissions);
       }
-      if (missing.length) {
-        throw new PermissionsError(missing);
-      }
-
       return method.call(this, channelID, ...args);
     };
   };
-};
+
+const computeMissingPermissions = (
+  currentUserPermissions: bigint,
+  permissions: Permissions[],
+) =>
+  permissions.filter((permission) =>
+    (currentUserPermissions & PermissionFlagsBits[permission]) !==
+      PermissionFlagsBits[permission]
+  );
