913d7445474691aa1c008cc283169b3641f0954e
Apacheopteryx <Idevtanm@gmail.com>
2021-05-03 21:24:13 -0500

    etf soon maybe?

---
+++ lib/util/etf_codec.ts
@@ -0,0 +1,3 @@
+export const pack = () => {};
+
+export const unpack = () => {};
--- lib/util/mod.ts
+++ lib/util/mod.ts
@@ -1,5 +1,6 @@
 export * from "./async_event_target.ts";
 export * from "./cdn.ts";
+export * from "./etf_codec.ts";
 export * as logger from "./logger.ts";
 export * from "./rate_limit_bucket.ts";
 export * from "./uint8_codec.ts";
