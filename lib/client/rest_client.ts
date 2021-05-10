a4dcc7a5fa4066a7b605cf244878f3a176387140
Apacheopteryx <Idevtanm@gmail.com>
2021-05-09 22:32:09 -0500

    finish adding decorated methods

--- lib/client/rest_client.ts
+++ lib/client/rest_client.ts
@@ -2,18 +2,33 @@ import type {
   RESTGetAPIAuditLogQuery,
   RESTGetAPIChannelMessageReactionUsersQuery,
   RESTGetAPIChannelMessagesQuery,
+  RESTGetAPIGuildPruneCountQuery,
   RESTPatchAPIChannelJSONBody,
   RESTPatchAPIChannelMessageJSONBody,
+  RESTPatchAPICurrentGuildMemberNicknameJSONBody,
   RESTPatchAPIGuildChannelPositionsJSONBody,
   RESTPatchAPIGuildEmojiJSONBody,
   RESTPatchAPIGuildJSONBody,
+  RESTPatchAPIGuildMemberJSONBody,
+  RESTPatchAPIGuildRoleJSONBody,
+  RESTPatchAPIGuildRolePositionsJSONBody,
+  RESTPatchAPIGuildTemplateJSONBody,
+  RESTPatchAPIGuildWelcomeScreenJSONBody,
+  RESTPatchAPIGuildWidgetSettingsJSONBody,
+  RESTPatchAPIWebhookJSONBody,
   RESTPostAPIChannelFollowersJSONBody,
   RESTPostAPIChannelInviteJSONBody,
   RESTPostAPIChannelMessageJSONBody,
   RESTPostAPIChannelMessagesBulkDeleteJSONBody,
+  RESTPostAPIChannelWebhookJSONBody,
   RESTPostAPIGuildChannelJSONBody,
   RESTPostAPIGuildEmojiJSONBody,
+  RESTPostAPIGuildPruneJSONBody,
+  RESTPostAPIGuildRoleJSONBody,
+  RESTPostAPIGuildTemplatesJSONBody,
   RESTPutAPIChannelPermissionJSONBody,
+  RESTPutAPIGuildBanJSONBody,
+  RESTPutAPIGuildMemberJSONBody,
 } from "./deps.ts";
 import {
   channelPermissionsDecorator,
@@ -244,15 +259,6 @@ export class RESTClient extends HTTPClient {
 
   // getPinnedMessages
 
-  @guildPermissionsDecorator(["KICK_MEMBERS"])
-  removeGuildMember(
-    guildID: ActualSnowflake,
-    userID: ActualSnowflake,
-    reason?: string,
-  ) {
-    return super.removeGuildMember(guildID, userID, reason);
-  }
-
   @channelPermissionsDecorator(["MANAGE_MESSAGES", "VIEW_CHANNEL"])
   addPinnedChannelMessage(
     channelID: ActualSnowflake,
@@ -359,4 +365,300 @@ export class RESTClient extends HTTPClient {
   // getGuildMember
   // getGuildMembers
   // searchGuildMember
+
+  @guildPermissionsDecorator(["CREATE_INSTANT_INVITE"])
+  addGuildMember(
+    guildID: bigint,
+    userID: ActualSnowflake,
+    data: RESTPutAPIGuildMemberJSONBody,
+  ) {
+    return super.addGuildMember(guildID, userID, data);
+  }
+
+  @guildPermissionsDecorator(["MOVE_MEMBERS", "MUTE_MEMBERS", "DEAFEN_MEMBERS"])
+  editGuildMember(
+    guildID: ActualSnowflake,
+    userID: ActualSnowflake,
+    data: RESTPatchAPIGuildMemberJSONBody,
+    reason?: string,
+  ) {
+    return super.editGuildMember(guildID, userID, data, reason);
+  }
+
+  @guildPermissionsDecorator(["CHANGE_NICKNAME"])
+  editCurrentUserNick(
+    guildID: ActualSnowflake,
+    data: RESTPatchAPICurrentGuildMemberNicknameJSONBody,
+  ) {
+    return super.editCurrentUserNick(guildID, data);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_ROLES"])
+  addGuildMemberRole(
+    guildID: ActualSnowflake,
+    userID: ActualSnowflake,
+    roleID: ActualSnowflake,
+    reason?: string,
+  ) {
+    return super.addGuildMemberRole(guildID, userID, roleID, reason);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_ROLES"])
+  removeGuildMemberRole(
+    guildID: ActualSnowflake,
+    userID: ActualSnowflake,
+    roleID: ActualSnowflake,
+    reason?: string,
+  ) {
+    return super.removeGuildMemberRole(guildID, userID, roleID, reason);
+  }
+
+  @guildPermissionsDecorator(["KICK_MEMBERS"])
+  removeGuildMember(
+    guildID: ActualSnowflake,
+    userID: ActualSnowflake,
+    reason?: string,
+  ) {
+    return super.removeGuildMember(guildID, userID, reason);
+  }
+
+  @guildPermissionsDecorator(["BAN_MEMBERS"])
+  getGuildBans(guildID: ActualSnowflake) {
+    return super.getGuildBans(guildID);
+  }
+
+  @guildPermissionsDecorator(["BAN_MEMBERS"])
+  getGuildBan(guildID: ActualSnowflake, userID: ActualSnowflake) {
+    return super.getGuildBan(guildID, userID);
+  }
+
+  @guildPermissionsDecorator(["BAN_MEMBERS"])
+  createGuildBan(
+    guildID: ActualSnowflake,
+    userID: ActualSnowflake,
+    data: RESTPutAPIGuildBanJSONBody,
+    reason?: string,
+  ) {
+    return super.createGuildBan(guildID, userID, data, reason);
+  }
+
+  @guildPermissionsDecorator(["BAN_MEMBERS"])
+  removeGuildBan(
+    guildID: ActualSnowflake,
+    userID: ActualSnowflake,
+    reason?: string,
+  ) {
+    return super.removeGuildBan(guildID, userID, reason);
+  }
+
+  // getGuildRoles
+
+  @guildPermissionsDecorator(["MANAGE_ROLES"])
+  createGuildRole(
+    guildID: ActualSnowflake,
+    data: RESTPostAPIGuildRoleJSONBody,
+    reason?: string,
+  ) {
+    return super.createGuildRole(guildID, data, reason);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_ROLES"])
+  editGuildRolePositions(
+    guildID: ActualSnowflake,
+    data: RESTPatchAPIGuildRolePositionsJSONBody,
+  ) {
+    return super.editGuildRolePositions(guildID, data);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_ROLES"])
+  editGuildRole(
+    guildID: ActualSnowflake,
+    roleID: ActualSnowflake,
+    data: RESTPatchAPIGuildRoleJSONBody,
+    reason?: string,
+  ) {
+    return super.editGuildRole(guildID, roleID, data, reason);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_ROLES"])
+  deleteGuildRole(
+    guildID: ActualSnowflake,
+    roleID: ActualSnowflake,
+    reason?: string,
+  ) {
+    return super.deleteGuildRole(guildID, roleID, reason);
+  }
+
+  @guildPermissionsDecorator(["KICK_MEMBERS"])
+  getGuildPruneCount(
+    guildID: ActualSnowflake,
+    query: RESTGetAPIGuildPruneCountQuery,
+  ) {
+    return super.getGuildPruneCount(guildID, query);
+  }
+
+  @guildPermissionsDecorator(["KICK_MEMBERS"])
+  beginGuildPrune(
+    guildID: ActualSnowflake,
+    query: RESTPostAPIGuildPruneJSONBody,
+    reason?: string,
+  ) {
+    return super.beginGuildPrune(guildID, query, reason);
+  }
+
+  // getGuildVoiceRegions
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  getGuildInvites(guildID: ActualSnowflake) {
+    return super.getGuildInvites(guildID);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  getGuildIntegrations(guildID: ActualSnowflake) {
+    return super.getGuildIntegrations(guildID);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  deleteGuildIntegration(
+    guildID: ActualSnowflake,
+    integrationID: ActualSnowflake,
+    reason?: string,
+  ) {
+    return super.deleteGuildIntegration(guildID, integrationID, reason);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  getGuildWidgetSettings(guildID: ActualSnowflake) {
+    return super.getGuildWidgetSettings(guildID);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  editGuildWidget(
+    guildID: ActualSnowflake,
+    data: RESTPatchAPIGuildWidgetSettingsJSONBody,
+  ) {
+    return super.editGuildWidget(guildID, data);
+  }
+
+  // getGuildWidget
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  getGuildVanityURL(guildID: ActualSnowflake) {
+    return super.getGuildVanityURL(guildID);
+  }
+
+  // getGuildWidgetImage
+  // getGuildWelcomeScreen
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  editGuildWelcomeScreen(
+    guildID: ActualSnowflake,
+    data: RESTPatchAPIGuildWelcomeScreenJSONBody,
+    reason?: string,
+  ) {
+    return super.editGuildWelcomeScreen(guildID, data, reason);
+  }
+
+  // updateCurrentUserVoiceState
+  // updateUserVoiceState
+  // getInvite
+
+  @guildPermissionsDecorator(["MANAGE_CHANNELS" /* "MANAGE_GUILD", */])
+  deleteInvite(inviteCode: string, reason?: string) {
+    return super.deleteInvite(inviteCode, reason);
+  }
+
+  // getGuildTemplate
+  // createGuildfromTemplate
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  getGuildTemplates(guildID: ActualSnowflake) {
+    return super.getGuildTemplates(guildID);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  createGuildTemplate(
+    guildID: ActualSnowflake,
+    data: RESTPostAPIGuildTemplatesJSONBody,
+  ) {
+    return super.createGuildTemplate(guildID, data);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  syncGuildTemplate(guildID: ActualSnowflake, templateCode: string) {
+    return super.syncGuildTemplate(guildID, templateCode);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  editGuildTemplate(
+    guildID: ActualSnowflake,
+    templateCode: string,
+    data: RESTPatchAPIGuildTemplateJSONBody,
+  ) {
+    return super.editGuildTemplate(guildID, templateCode, data);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_GUILD"])
+  deleteGuildTemplate(guildID: ActualSnowflake, templateCode: string) {
+    return super.deleteGuildTemplate(guildID, templateCode);
+  }
+
+  // getCurrentUser
+  // getUser
+  // editCurrentUser
+  // getCurrentUserGuilds
+  // leaveGuild
+  // createPrivateChannel
+  // createGroupPrivateChannel
+  // getUserConnections
+  // getVoiceRegions
+
+  @guildPermissionsDecorator(["MANAGE_WEBHOOKS"])
+  createWebhook(
+    channelID: ActualSnowflake,
+    data: RESTPostAPIChannelWebhookJSONBody,
+  ) {
+    return super.createWebhook(channelID, data);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_WEBHOOKS"])
+  getChannelWebhooks(channelID: ActualSnowflake) {
+    return super.getChannelWebhooks(channelID);
+  }
+
+  @guildPermissionsDecorator(["MANAGE_WEBHOOKS"])
+  getGuildWebhooks(guildID: ActualSnowflake) {
+    return super.getGuildWebhooks(guildID);
+  }
+
+  // getWebhook
+  // gerWebhookwithToken
+
+  @guildPermissionsDecorator(["MANAGE_WEBHOOKS"])
+  editWebhook(
+    webhookID: ActualSnowflake,
+    data: RESTPatchAPIWebhookJSONBody,
+    reason?: string,
+  ) {
+    return super.editWebhook(webhookID, data, reason);
+  }
+
+  // editWebhookwithToken
+
+  @guildPermissionsDecorator(["MANAGE_WEBHOOKS"])
+  deleteWebhook(webhookID: ActualSnowflake, reason?: string) {
+    return super.deleteWebhook(webhookID, reason);
+  }
+
+  // deleteWebhookwithToken
+  // executeWebhook
+  // executeSlackCompatibleWebhook
+  // executeGitHubCompatibleWebhook
+  // getWebhookMessage
+  // editWebhookMessage
+  // deleteWebhookMessage
+  // getGateway
+  // getGatewayBot
+  // getCurrentBotApplicationInformation
+  // getCurrentAuthorizationInformation
 }
