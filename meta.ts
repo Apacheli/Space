af920612a4c02a6d12b645088d94a3c7ceb7b38c
Apacheopteryx <Idevtanm@gmail.com>
2021-05-12 01:12:43 -0500

    upgrade deno meta to 1.10.1

--- meta.ts
+++ meta.ts
@@ -1,6 +1,6 @@
 // deno-fmt-ignore-line
 export const
-  deno = "1.9.2",
+  deno = "1.10.1",
   repo = "https://github.com/Apacheli/Space",
   version = "0.8.0-alpha",
   mod = `https://deno.land/x/space@${version}`,
