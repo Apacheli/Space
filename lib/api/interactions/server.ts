ddb1d260ce1b1d3face0dbc218d43b3dea3b6fd2
Apacheopteryx <Idevtanm@gmail.com>
2021-05-12 17:40:13 -0500

    verifyKey

--- lib/api/interactions/server.ts
+++ lib/api/interactions/server.ts
@@ -5,64 +5,78 @@ import {
   InteractionType,
   readAll,
   serve,
+  Server,
   ServerRequest,
   Status,
   verify,
 } from "./deps.ts";
 import { AsyncEventTarget, decode, encode } from "../../util/mod.ts";
 
-export const headers = new Headers();
+const headers = new Headers();
 headers.set("content-type", "application/json");
 
-const respond = (req: ServerRequest, body: unknown, status = Status.OK) =>
-  req.respond({ body: JSON.stringify(body), status, headers });
+const respond = (request: ServerRequest, body: unknown, status = Status.OK) =>
+  request.respond({ body: JSON.stringify(body), status, headers });
 
-export class Server extends AsyncEventTarget {
+export class InteractionsServer extends AsyncEventTarget {
+  #server?: Server;
+
   constructor(public publicKey: string) {
     super();
   }
 
   async connect(port: number) {
-    const server = serve({ port });
-    for await (const req of server) {
-      this.onRequest(req);
+    for await (const request of this.#server = serve({ port })) {
+      this.onServerRequest(request);
     }
   }
 
-  private async onRequest(req: ServerRequest) {
-    const signature = req.headers.get("X-Signature-Ed25519");
-    const timestamp = req.headers.get("X-Signature-Timestamp");
+  close() {
+    if (!this.#server) {
+      throw new Error("No server");
+    }
+    this.#server.close();
+  }
+
+  private async onServerRequest(request: ServerRequest) {
+    const signature = request.headers.get("X-Signature-Ed25519");
+    const timestamp = request.headers.get("X-Signature-Timestamp");
 
     if (!(signature && timestamp)) {
-      return respond(req, "bad request", Status.BadRequest);
+      return respond(request, "bad request", Status.BadRequest);
     }
 
-    const body = await readAll(req.body);
+    const body = await readAll(request.body);
 
-    const isVerified = verify(
-      decodeString(this.publicKey),
-      decodeString(signature),
-      Uint8Array.from([...encode(timestamp), ...body]),
-    );
-
-    if (!isVerified) {
-      return respond(req, "invalid request signature", Status.Unauthorized);
+    if (!verifyKey(this.publicKey, signature, timestamp, body)) {
+      return respond(request, "invalid request signature", Status.Unauthorized);
     }
 
     const interaction: APIInteraction = JSON.parse(decode(body));
+    const respondd = (body: unknown) => respond(request, body);
 
     switch (interaction.type) {
       case InteractionType.Ping: {
-        return respond(req, { type: InteractionResponseType.Pong });
+        return respondd({ type: InteractionResponseType.Pong });
       }
-
       case InteractionType.ApplicationCommand: {
-        return this.dispatch("COMMAND", interaction, respond.bind(null, req));
+        return this.dispatch("COMMAND", interaction, respondd);
       }
-
-      case 3: { // buttons
-        return this.dispatch("BUTTON", interaction, respond.bind(null, req));
+      case 3: { // Buttons/Components
+        return this.dispatch("BUTTON", interaction, respondd);
       }
     }
   }
 }
+
+export const verifyKey = (
+  publicKey: string,
+  signature: string,
+  timestamp: string,
+  uint8: Uint8Array,
+) =>
+  verify(
+    decodeString(publicKey),
+    decodeString(signature),
+    Uint8Array.from([...encode(timestamp), ...uint8]),
+  );
