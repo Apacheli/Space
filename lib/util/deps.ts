d157b18f9163a4441cf561bb0767f0e8bcd3dc99
Apacheopteryx <Idevtanm@gmail.com>
2021-05-14 00:08:19 -0500

    update etf deps

--- lib/util/deps.ts
+++ lib/util/deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/x/discord_api_types@0.18.0/globals.ts";
 export * from "https://deno.land/x/discord_api_types@0.18.0/payloads/v8/permissions.ts";
-export * from "https://deno.land/x/void@1.0.0/mod.ts";
+export * from "https://deno.land/x/void@1.0.1/mod.ts";
