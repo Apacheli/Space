46588c0b1f009826ebd91a46a68b4b76efbe61f9
Apacheopteryx <Idevtanm@gmail.com>
2021-05-16 01:59:44 -0500

    How to make discord bot

--- dev/tag_format.ts
+++ dev/tag_format.ts
@@ -4,6 +4,6 @@ const tagReleases = RELEASES
   .replace(/\[`\w{7}`\]\((.+?)\)/g, "$1")
   .replace(/ +/g, " ");
 
-console.log(`Parsing RELEASES for GitHub tag format compatibility...`);
+console.log(`Parsing 'RELEASES.md' for GitHub tag format compatibility...`);
 await Deno.writeTextFile("RELEASES_new.md", tagReleases);
 console.log("Done. See 'RELEASES_new.md' for tag version.");
