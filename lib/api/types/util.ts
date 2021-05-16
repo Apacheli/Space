256125af61a0a4110da0fc356dbf320d4dac3ae3
Apacheopteryx <Idevtanm@gmail.com>
2021-05-16 13:45:22 -0500

    fix nullify type

--- lib/api/types/util.ts
+++ lib/api/types/util.ts
@@ -2,6 +2,4 @@ export type Nullable<T> = {
   [K in keyof T]: T[K] | null;
 };
 
-export type Nullify<T, K extends keyof T> = {
-  [X in K]: T[X] | null;
-};
+export type Nullify<T, K extends keyof T> = Omit<T, K> & Nullable<Pick<T, K>>;
