21081550494b8ef83dced0b8e07fea202affc1f5
Apacheopteryx <Idevtanm@gmail.com>
2021-05-16 13:41:59 -0500

    ok ok ok ok

--- lib/api/types/interactions/slash_commands.ts
+++ lib/api/types/interactions/slash_commands.ts
@@ -1,24 +1,35 @@
-import {} from "../resources/webhook.ts";
+import type { Snowflake } from "../reference.ts";
+import type {
+  AllowedMentions,
+  Channel,
+  Embed,
+  Message,
+} from "../resources/channel.ts";
+import type { GuildMember } from "../resources/guild.ts";
+import type { User } from "../resources/user.ts";
+import type {
+  EditWebhookMessageJSON,
+  ExecuteWebhookJSON,
+  GetWebhookMessageBody,
+} from "../resources/webhook.ts";
+import type { Role } from "../topics/permissions.ts";
+import type { Nullify } from "../util.ts";
 
 /** https://discord.com/developers/docs/interactions/slash-commands#get-global-application-commands */
 export type GetGlobalApplicationCommandsBody = ApplicationCommand[];
 
 /** https://discord.com/developers/docs/interactions/slash-commands#create-global-application-command */
-export interface CreateGlobalApplicationCommandJSON {
-  name: string;
-  description: string;
-  options?: ApplicationCommandOption[];
-  default_permission?: boolean;
-}
+export type CreateGlobalApplicationCommandJSON = Omit<
+  ApplicationCommand,
+  "id" | "application_id"
+>;
 
 export type CreateGlobalApplicationCommandBody = ApplicationCommand;
 
 export type GetGlobalApplicationCommandBody = ApplicationCommand;
 
 export type EditGlobalApplicationCommandJSON = Partial<
-  CreateGlobalApplicationCommandJSON & {
-    options: ApplicationCommandOption[] | null;
-  }
+  Nullify<CreateGlobalApplicationCommandJSON, "options">
 >;
 
 export type EditGlobalApplicationCommandBody = ApplicationCommand;
@@ -91,3 +102,121 @@ export interface EditApplicationCommandPermissionsJSON {
 
 export type BatchEditApplicationCommandPermissions =
   GuildApplicationCommandPermissions[];
+
+export interface ApplicationCommand {
+  id: Snowflake;
+  application_id: Snowflake;
+  name: string;
+  description: string;
+  options?: ApplicationCommandOption[];
+  default_permission?: boolean;
+}
+
+export interface ApplicationCommandOption {
+  type: ApplicationCommandOptionType;
+  name: string;
+  description: string;
+  required?: boolean;
+  choices?: ApplicationCommandOptionChoice[];
+  options?: ApplicationCommandOption[];
+}
+
+export enum ApplicationCommandOptionType {
+  SubCommand = 1,
+  SubCommandGroup,
+  String,
+  Integer,
+  Boolean,
+  User,
+  Channel,
+  Role,
+  Mentionable,
+}
+
+export interface ApplicationCommandOptionChoice {
+  name: string;
+  value: string | number;
+}
+
+export interface GuildApplicationCommandPermissions {
+  id: Snowflake;
+  application_id: Snowflake;
+  guild_id: Snowflake;
+  permissions: ApplicationCommandPermissions[];
+}
+
+export interface ApplicationCommandPermissions {
+  id: Snowflake;
+  type: ApplicationCommandPermissionType;
+  permission: boolean;
+}
+
+export enum ApplicationCommandPermissionType {
+  Role = 1,
+  User,
+}
+
+export interface Interaction {
+  id: Snowflake;
+  application_id: Snowflake;
+  type: InteractionType;
+  data?: ApplicationCommandInteractionData;
+  guild_id?: Snowflake;
+  channel_id?: Snowflake;
+  member?: GuildMember;
+  user?: User;
+  token: string;
+  version: number;
+}
+
+export enum InteractionType {
+  Ping = 1,
+  ApplicationCommand = 2,
+}
+
+export interface ApplicationCommandInteractionData {
+  id: Snowflake;
+  name: string;
+  resolved?: ApplicationCommandInteractionDataResolved;
+  options?: ApplicationCommandInteractionDataOption[];
+}
+
+export interface ApplicationCommandInteractionDataResolved {
+  users?: Record<Snowflake, User>[];
+  members?: Record<Snowflake, GuildMember>[];
+  roles?: Record<Snowflake, Role>[];
+  channels?: Record<Snowflake, Channel>[];
+}
+
+export interface ApplicationCommandInteractionDataOption {
+  name: string;
+  type: ApplicationCommandOptionType;
+  value?: unknown;
+  options?: ApplicationCommandInteractionDataOption[];
+}
+
+export interface InteractionResponse {
+  type: InteractionCallbackType;
+  data?: InteractionApplicationCOmmandCallbackData;
+}
+
+export enum InteractionCallbackType {
+  Pong = 1,
+  ChannelMessageWithSource = 4,
+  DeferredChannelMessageWithSource,
+}
+
+export interface InteractionApplicationCOmmandCallbackData {
+  tts?: boolean;
+  content?: string;
+  embeds?: Embed[];
+  allowed_mentions?: AllowedMentions;
+  flags?: 64;
+}
+
+export interface MessageInteraction {
+  id: Snowflake;
+  type: InteractionType;
+  name: string;
+  user: User;
+}
--- lib/api/types/util.ts
+++ lib/api/types/util.ts
@@ -1,3 +1,7 @@
 export type Nullable<T> = {
   [K in keyof T]: T[K] | null;
 };
+
+export type Nullify<T, K extends keyof T> = {
+  [X in K]: T[X] | null;
+};
