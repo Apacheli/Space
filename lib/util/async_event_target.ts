6449da7d1fe8072a23886ebe43c282b4e6d925c6
Apacheopteryx <Idevtanm@gmail.com>
2021-05-18 17:32:04 -0500

    async event target fixes

--- lib/util/async_event_target.ts
+++ lib/util/async_event_target.ts
@@ -4,44 +4,40 @@ export interface AsyncEventTargetReceiveOptions<T> {
   limit?: number;
 }
 
-export interface ListenerStream<T> {
+export interface Listener<T> {
   readable: ReadableStream<T>;
   writer: WritableStreamDefaultWriter<T>;
-  [Symbol.asyncIterator]: () => AsyncIterableIterator<T>;
 }
 
-export class AsyncEventTarget<T> extends Map<string, ListenerStream<T[]>[]> {
-  listen(event: string): ListenerStream<T[]> {
+export class AsyncEventTarget<T> extends Map<string, Listener<T[]>[]> {
+  listen(event: string) {
     const { readable, writable } = new TransformStream<T[], T[]>();
-    const listener: ListenerStream<T[]> = {
-      readable,
-      writer: writable.getWriter(),
-      [Symbol.asyncIterator]: () => readable[Symbol.asyncIterator](),
-    };
+    const listener = { readable, writer: writable.getWriter() };
     if (this.get(event)?.push(listener) === undefined) {
       this.set(event, [listener]);
     }
-    return listener;
+    return readable;
   }
 
-  deafen(event: string, listener?: ListenerStream<T[]>) {
-    const streams = this.get(event);
-    if (!streams) {
+  deafen(event: string, readable?: ReadableStream<T[]>) {
+    const listeners = this.get(event);
+    if (!listeners) {
       return;
     }
-    if (listener) {
-      if (streams.splice(streams.indexOf(listener), 1).length > 0) {
-        listener.writer.close();
+    if (readable) {
+      const index = listeners.findIndex((l) => l.readable === readable);
+      if (index !== -1) {
+        listeners.splice(index, 1)[0].writer.close();
       }
       return;
     }
     this.delete(event);
-    streams.forEach((listener) => listener.writer.close());
+    listeners.forEach(({ writer }) => writer.close());
   }
 
   dispatch(event: string, ...args: T[]) {
-    const writers = this.get(event);
-    writers?.forEach(({ writer }) => writer.write(args));
+    const listeners = this.get(event);
+    listeners?.forEach(({ writer }) => writer.write(args));
   }
 
   async receive(event: string, {
@@ -49,10 +45,10 @@ export class AsyncEventTarget<T> extends Map<string, ListenerStream<T[]>[]> {
     filter,
     limit = 1,
   }: AsyncEventTargetReceiveOptions<T> = {}) {
-    const listener = this.listen(event);
-    const reader = listener.readable.getReader();
+    const readable = this.listen(event);
+    const reader = readable.getReader();
     const received = new Array<T[]>(limit);
-    const timeout = setTimeout(() => this.deafen(event, listener), delay);
+    const timeout = setTimeout(() => this.deafen(event, readable), delay);
     for (let i = 0; i < limit;) {
       const { done, value } = await reader.read();
       if (done) {
@@ -62,7 +58,7 @@ export class AsyncEventTarget<T> extends Map<string, ListenerStream<T[]>[]> {
       }
     }
     clearTimeout(timeout);
-    this.deafen(event, listener);
+    this.deafen(event, readable);
     return received;
   }
 }
