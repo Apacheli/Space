a74de13f7e5cff3143c651c21feab2367f373573
Apacheopteryx <Idevtanm@gmail.com>
2021-05-20 17:39:43 -0500

    team types

--- lib/api/types/topics/teams.ts
+++ lib/api/types/topics/teams.ts
@@ -1,2 +1,38 @@
+// deno-fmt-ignore-file camelcase
+
+// https://discord.com/developers/docs/topics/teams
+
+import type { Snowflake } from "../reference.ts";
+import type { User } from "../resources/user.ts";
+
+/** https://discord.com/developers/docs/topics/teams#data-models-team-object */
 export interface Team {
+  /** a hash of the image of the team's icon */
+  icon: string | null;
+  /** the unique id of the team */
+  id: Snowflake;
+  /** the members of the team */
+  members: TeamMember[];
+  /** the name of the team */
+  name: string;
+  /** the user id of the current team owner */
+  owner_user_id: Snowflake;
+}
+
+/** https://discord.com/developers/docs/topics/teams#data-models-team-members-object */
+export interface TeamMember {
+  /** the user's [membership state](https://discord.com/developers/docs/topics/teams#data-models-membership-state-enum) on the team */
+  membership_state: MembershipState;
+  /** will always be `["*"]` */
+  permissions: string[];
+  /** 	the id of the parent team of which they are a member */
+  team_id: Snowflake;
+  /** the avatar, discriminator, id, and username of the user */
+  user: Partial<User>;
+}
+
+/** https://discord.com/developers/docs/topics/teams#data-models-membership-state-enum */
+export enum MembershipState {
+  Invited = 1,
+  Accepted
 }
