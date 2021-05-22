5ff97d3fa8fc79fa3450f3234a480f481b9a48a0
Apacheopteryx <Idevtanm@gmail.com>
2021-05-22 09:25:04 -0500

    oauth2 typings

--- lib/api/types/reference.ts
+++ lib/api/types/reference.ts
@@ -1,2 +1,5 @@
+/** https://discord.com/developers/docs/topics/permissions */
+export type Permissions = `${bigint}`;
+
 /** https://discord.dev/reference#snowflakes */
 export type Snowflake = `${bigint}`;
