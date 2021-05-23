91e557346dd1a429bce62aca8e5b872fa73cca9d
Apacheopteryx <Idevtanm@gmail.com>
2021-05-23 17:18:44 -0500

    pepega

--- lib/api/types/topics/gateway.ts
+++ lib/api/types/topics/gateway.ts
@@ -15,9 +15,9 @@ export interface GatewayPayload<op extends GatewayOpcodes, d extends unknown> {
   /** event data */
   d: d;
   /** sequence number, used for resuming sessions and heartbeats */
-  s?: number;
+  s: number | null;
   /** the event name for this payload */
-  t?: string;
+  t: string | null;
 }
 
 /** https://discord.com/developers/docs/topics/gateway#connecting-gateway-url-query-string-params */
@@ -28,6 +28,110 @@ export interface GatewayURLQuery {
   encoding: "etf" | "json";
   /** The (optional) compression of gateway packets */
   compress?: "zlib-stream";
+}
+
+/** https://discord.com/developers/docs/topics/gateway#commands-and-events-gateway-events */
+export enum GatewayEvents {
+  /** defines the heartbeat interval */
+  Hello = "HELLO",
+  /** contains the initial state information */
+  Ready = "READY",
+  /** response to [Resume](https://discord.com/developers/docs/topics/gateway#resume) */
+  Resumed = "RESUMED",
+  /** new [Slash Command](https://discord.com/developers/docs/interactions/slash-commands) was created */
+  ApplicationCommandCreate = "APPLICATION_COMMAND_CREATE",
+  /** [Slash Command](https://discord.com/developers/docs/interactions/slash-commands) was updated */
+  ApplicationCommandUpdate = "APPLICATION_COMMAND_UPDATE",
+  /** [Slash Command](https://discord.com/developers/docs/interactions/slash-commands) was deleted */
+  ApplicationCommandDelete = "APPLICATION_COMMAND_DELETE",
+  /** new guild channel created */
+  ChannelCreate = "CHANNEL_CREATE",
+  /** channel was updated */
+  ChannelUpdate = "CHANNEL_UPDATE",
+  /** channel was deleted */
+  ChannelDelete = "CHANNEL_DELETE",
+  /** message was pinned or unpinned */
+  ChannelPinsUpdate = "CHANNEL_PINS_UPDATE",
+  /** thread created, also sent when being added to a private thread */
+  ThreadCreate = "THREAD_CREATE",
+  /** thread was updated */
+  ThreadUpdate = "THREAD_UPDATE",
+  /** thread was deleted */
+  ThreadDelete = "THREAD_DELETE",
+  /** sent when gaining access to a channel, contains all active threads in that channel */
+  ThreadListSync = "THREAD_LIST_SYNC",
+  /** [thread member](https://discord.com/developers/docs/resources/channel#thread-member-object) for the current user was updated */
+  ThreadMemberUpdate = "THREAD_MEMBER_UPDATE",
+  /** some user(s) were added to or removed from a thread */
+  ThreadMembersUpdate = "THREAD_MEMBERS_UPDATE",
+  /** lazy-load for unavailable guild, guild became available, or user joined a new guild */
+  GuildCreate = "GUILD_CREATE",
+  /** guild was updated */
+  GuildUpdate = "GUILD_UPDATE",
+  /** guild became unavailable, or user left/was removed from a guild */
+  GuildDelete = "GUILD_DELETE",
+  /** user was banned from a guild */
+  GuildBanAdd = "GUILD_BAN_ADD",
+  /** user was unbanned from a guild */
+  GuildBanRemove = "GUILD_BAN_REMOVE",
+  /** guild emojis were updated */
+  GuildEmojisUpdate = "GUILD_EMOJIS_UPDATE",
+  /** guild integration was updated */
+  GuildIntegrationsUpdate = "GUILD_INTEGRATIONS_UPDATE",
+  /** new user joined a guild */
+  GuildMemberAdd = "GUILD_MEMBER_ADD",
+  /** user was removed from a guild */
+  GuildMemberRemove = "GUILD_MEMBER_REMOVE",
+  /** guild member was updated */
+  GuildMemberUpdate = "GUILD_MEMBER_UPDATE",
+  /** response to [Request Guild Members](https://discord.com/developers/docs/topics/gateway#request-guild-members) */
+  GuildMembersChunk = "GUILD_MEMBERS_CHUNK",
+  /** guild role was created */
+  GuildRoleCreate = "GUILD_ROLE_CREATE",
+  /** guild role was updated */
+  GuildRoleUpdate = "GUILD_ROLE_UPDATE",
+  /** guild role was deleted */
+  GuildRoleDelete = "GUILD_ROLE_DELETE",
+  /** guild integration was created */
+  IntegrationCreate = "INTEGRATION_CREATE",
+  /** guild integration was updated */
+  IntegrationUpdate = "INTEGRATION_UPDATE",
+  /** guild integration was deleted */
+  IntegrationDelete = "INTEGRATION_DELETE",
+  /** user used an interaction, such as a [Slash Command](https://discord.com/developers/docs/interactions/slash-commands) */
+  InteractionCreate = "INTERACTION_CREATE",
+  /** invite to a channel was created */
+  InviteCreate = "INVITE_CREATE",
+  /** invite to a channel was deleted */
+  InviteDelete = "INVITE_DELETE",
+  /** message was created */
+  MessageCreate = "MESSAGE_CREATE",
+  /** message was edited */
+  MessageUpdate = "MESSAGE_UPDATE",
+  /** message was deleted */
+  MessageDelete = "MESSAGE_DELETE",
+  /** multiple messages were deleted at once */
+  MessageDeleteBulk = "MESSAGE_DELETE_BULK",
+  /** user reacted to a message */
+  MessageReactionAdd = "MESSAGE_REACTION_ADD",
+  /** user removed a reaction from a message */
+  MessageReactionRemove = "MESSAGE_REACTION_REMOVE",
+  /** all reactions were explicitly removed from a message */
+  MessageReactionRemoveAll = "MESSAGE_REACTION_REMOVE_ALL",
+  /** all reactions for a given emoji were explicitly removed from a message */
+  MessageReactionRemoveEmoji = "MESSAGE_REACTION_REMOVE_EMOJI",
+  /** user was updated */
+  PresenceUpdate = "PRESENCE_UPDATE",
+  /** user started typing in a channel */
+  TypingStart = "TYPING_START",
+  /** properties about the user changed */
+  UserUpdate = "USER_UPDATE",
+  /** someone joined, left, or moved a voice channel */
+  VoiceStateUpdate = "VOICE_STATE_UPDATE",
+  /** guild's voice server was updated */
+  VoiceServerUpdate = "VOICE_SERVER_UPDATE",
+  /** guild channel webhook was created, update, or deleted */
+  WebhooksUpdate = "WEBHOOKS_UPDATE",
 }
 
 /** https://discord.com/developers/docs/topics/gateway#identify */
@@ -162,4 +266,9 @@ export type HelloPayload = GatewayPayload<
 export interface HelloPayloadData {
   /** the interval (in milliseconds) the client should heartbeat with */
   heartbeat_interval: number;
+}
+
+export interface DispatchPayload<t extends string, d>
+  extends GatewayPayload<GatewayOpcodes.Dispatch, d> {
+  t: t;
 }
