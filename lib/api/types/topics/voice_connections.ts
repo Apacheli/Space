4a59a9c2d6c626a5fc24d5a354d980dc41fb3006
Apacheopteryx <Idevtanm@gmail.com>
2021-05-23 22:08:32 -0500

    more types and stuff

---
+++ lib/api/types/topics/voice_connections.ts
@@ -0,0 +1,4 @@
+// https://discord.com/developers/docs/topics/voice-connections
+
+/** https://discord.com/developers/docs/topics/voice-connections#voice-gateway-versioning */
+export type VoiceGatewayVersions = 4;
