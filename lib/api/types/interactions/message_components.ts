5fa2c892f39e18b808324d52d0ca7ea20e9584bc
Apacheopteryx <Idevtanm@gmail.com>
2021-05-27 09:34:12 -0500

    message components

---
+++ lib/api/types/interactions/message_components.ts
@@ -0,0 +1,61 @@
+// deno-lint-ignore-file camelcase
+
+// https://discord.dev/interactions/message-components
+
+/** https://discord.dev/interactions/message-components#component-object */
+export interface Component {
+  /** [component type](https://discord.dev/interactions/message-components#component-types) */
+  type: ComponentTypews;
+  /** one of [button styles](https://discord.dev/interactions/message-components#buttons-button-styles) */
+  style?: ButtonStyles;
+  /** text that appears on the button, max 80 characters */
+  label?: string;
+  /**` name`, `id`, and `animated` */
+  emoji?: Partial<Emoji>;
+  /** a developer-defined identifier for the button, max 100 characters */
+  custon_id?: string;
+  /** a url for link-style buttons */
+  url?: string;
+  /** whether the button is disabled, default `false` */
+  disabled?: boolean;
+}
+
+/** https://discord.dev/interactions/message-components#component-types */
+export enum ComponentTypes {
+  /** A container for other components */
+  ActionRow = 1,
+  /** A clickable button */
+  Button,
+}
+
+/** https://discord.dev/interactions/message-components#buttons */
+export interface Button {
+  /** `2` for a button */
+  type: number;
+  /** one of [button styles](https://discord.dev/interactions/message-components#buttons-button-styles) */
+  style: ButtonStyles;
+  /** text the appears on the button, max 80 characters */
+  label?: string;
+  /** `name`, `id`, and `animated` */
+  emoji?: Partial<Emoji>;
+  /** a developer-defined identifier for the button, max 100 characters */
+  custom_id?: string;
+  /** a url for link-style buttons */
+  url?: string;
+  /** whether the button is disabled, default `false` */
+  disabled?: boolean;
+}
+
+/** https://discord.dev/interactions/message-components#buttons-button-styles */
+export enum ButtonStyles {
+  /** blurple */
+  Primary = 1,
+  /** grey */
+  Secondary,
+  /** green */
+  Success,
+  /** red */
+  Danger,
+  /** grey, navigates to a URL */
+  Link,
+}
--- lib/api/types/interactions/mod.ts
+++ lib/api/types/interactions/mod.ts
@@ -1,1 +1,2 @@
+export * from "./message_components.ts";
 export * from "./slash_commands.ts";
