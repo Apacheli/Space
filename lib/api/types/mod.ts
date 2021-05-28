7a768a7f51d38ef275193455f9169d56f81dccff
Apacheopteryx <Idevtanm@gmail.com>
2021-05-27 21:37:42 -0500

    massive reworking to do...

--- lib/api/types/mod.ts
+++ lib/api/types/mod.ts
@@ -2,3 +2,4 @@ export * from "./game_sdk/mod.ts";
 export * from "./interactions/mod.ts";
 export * from "./resources/mod.ts";
 export * from "./topics/mod.ts";
+export * from "./reference.ts";
--- lib/structures/channels/news_channel.ts
+++ lib/structures/channels/news_channel.ts
@@ -5,14 +5,4 @@ import { TextChannel } from "./text_channel.ts";
  * Class representing a guild news channel (announcements) on Discord.
  */
 export class NewsChannel extends TextChannel {
-  /**
-   * https://discord.dev/resources/channel#follow-news-channel
-   *
-   * Follow a News Channel to send messages to a target channel. Requires the
-   * `MANAGE_WEBHOOKS` permission in the target channel. Returns a
-   * [followed channel](https://discord.dev/resources/channel#followed-channel-object) object.
-   */
-  follow(data: RESTPostAPIChannelFollowersJSONBody) {
-    return this.client.rest.followNewsChannel(this.id, data);
-  }
 }
