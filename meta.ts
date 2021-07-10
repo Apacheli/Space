b0bd53ecdd1f827ef0e086016ed7b387b935e41c
Apacheopteryx <Idevtanm@gmail.com>
2021-07-10 17:13:28 -0500

    bump

--- meta.ts
+++ meta.ts
@@ -1,6 +1,6 @@
 // deno-fmt-ignore-line
 export const
-  deno = "1.11.3",
-  repo = "https://github.com/spacelandx/space",
+  deno = "1.11.5",
+  repo = "https://github.com/apacheli/space",
   version = "0.11.0-alpha",
   url = `https://deno.land/x/space@${version}`;
